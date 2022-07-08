7ba6ae66826b828eb1f3cfb7f801e185471ea842
Chris Collings <CCollings@salesforce.com>
2022-07-08 10:00:27 -0300

    2.0.2

---
+++ Frameworks/archives/1_Cdp.xcarchive/Products/Library/Frameworks/Cdp.framework/Headers/Cdp.h
@@ -0,0 +1,19 @@
+//
+//  Cdp.h
+//  Cdp
+//
+//  Created by Tyler Roach on 6/29/20.
+//  Copyright © 2020 Salesforce Marketing Cloud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for cdp.
+FOUNDATION_EXPORT double cdpVersionNumber;
+
+//! Project version string for cdp.
+FOUNDATION_EXPORT const unsigned char cdpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cdp/PublicHeader.h>
+
+
