59bd18fbb1be5a51d949f7bdb18d795ffc61cd52
Chris Collings <CCollings@salesforce.com>
2023-02-03 11:22:09 -0400

    2.0.3

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url:"https://github.com/salesforce-marketingcloud/sfmc-sdk-ios",
-                 from: "1.0.4"
+                 from: "1.0.6"
         ),
     ],
     targets: [
